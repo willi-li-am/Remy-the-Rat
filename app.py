e84f62e21f71c4ed83a2ed05ed894c09a6a34891
yhchung12 <cyiuhim@gmail.com>
2024-09-15 00:44:35 -0400

    added request for flask backend

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 #do some flask shit
+
+from flask import Flask, request
+from robot import remy
+from pydub import AudioSegment
+
+app = Flask(__name__) 
+
+@app.route('/move_remy', methods=['GET'])
+def move_remy(): 
+    audio_path = request.args.get('audio_path')
+    audio = AudioSegment.from_file(audio_path)
+    audio_length = len(audio) / 1000.0
+    remy.fast_chopping(audio_length)
